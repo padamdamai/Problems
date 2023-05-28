014a30a804e9ae1007b58a2e31a5f7349726ed1c
padam damai <padamdamai888@gmail.com>
2023-05-28 13:24:26 +0545

    added Footer.js

---
+++ Footer.js
@@ -0,0 +1,6 @@
+import {React} from 'react';
+export default function Footer(){
+    return <div>
+         <footer><small>this is my footer documentation .you can visit here</small></footer>
+            </div>
+}
