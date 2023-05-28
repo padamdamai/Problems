8f4c20ea712515220261faf27a6fee11a1922e03
padamdamai <72964773+padamdamai@users.noreply.github.com>
2023-05-28 13:28:02 +0545

    Delete Footer.js

--- Footer.js
+++
@@ -1,6 +0,0 @@
-import {React} from 'react';
-export default function Footer(){
-    return <div>
-         <footer><small>this is my footer documentation .you can visit here</small></footer>
-            </div>
-}
