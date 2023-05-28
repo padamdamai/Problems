5c693b3625cf2438029bda6e48a3326346cc2809
padam damai <padamdamai888@gmail.com>
2023-05-28 13:26:17 +0545

    added header.js

---
+++ Header.js
@@ -0,0 +1,17 @@
+
+export default function Head(){
+    return <div>
+                <header>
+                   <nav className="nav">
+                   <img src="logoReact.png" className="img-logo"/><br/>
+                    <ul className="ul">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Information</li>
+                    <li>Form</li>
+                    </ul>
+                   </nav>
+                </header>
+            </div>
+}
